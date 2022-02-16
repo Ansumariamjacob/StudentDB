879ef2369d612e2cccfc29890991b616a1d64196
vanimnaoj <ansumariam99@gmail.com>
2022-02-16 15:13:38 +0530

    StudentDBApp completed

--- app/src/main/java/com/example/studentdbapp/AddStudent.java
+++ app/src/main/java/com/example/studentdbapp/AddStudent.java
@@ -13,12 +13,15 @@ public class AddStudent extends AppCompatActivity {
     EditText ed1,ed2,ed3,ed4;
     AppCompatButton b1,b2;
     String getName,getAddNo,getRollNo,getCollege;
+    DatabaseHelper dbHelper;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+        dbHelper=new DatabaseHelper(this);
+        dbHelper.getWritableDatabase();
         ed1=(EditText) findViewById(R.id.name);
         ed2=(EditText) findViewById(R.id.adno);
         ed3=(EditText) findViewById(R.id.rolno);
@@ -32,10 +35,20 @@ public class AddStudent extends AppCompatActivity {
                 getRollNo=ed3.getText().toString();
                 getAddNo=ed2.getText().toString();
                 getCollege=ed4.getText().toString();
-                Toast.makeText(getApplicationContext(),getName, Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),getRollNo,Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),getAddNo,Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(),getCollege,Toast.LENGTH_SHORT).show();
+              boolean result=dbHelper.insertValue(getName,getRollNo,getAddNo,getCollege);
+              if(result==true)
+              {
+                  ed1.setText("");
+                  ed2.setText("");
+                  ed3.setText("");
+                  ed4.setText("");
+                  Toast.makeText(getApplicationContext(),"Successfully inserted", Toast.LENGTH_SHORT).show();
+              }
+              else
+              {
+                  Toast.makeText(getApplicationContext(),"Failed to insert", Toast.LENGTH_SHORT).show();
+
+              }
 
             }
         });
